30226f118b177d7675b2082baaf0996290934116
Mohamed Abd elrhman <mohamed.abdelrahman.eltahawy@gmail.com>
2022-09-11 09:25:41 +0200

    Added Creative Speakers Section & Some Enhancements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { faUserGroup } from "@fortawesome/free-solid-svg-icons";
-import Overview from './design-system/components/Overview';
-
+import CreativeSpeakers from "./design-system/components/CreativeSpeakers/CreativeSpeakers";
+import speakerData from "./design-system/components/CreativeSpeakers/SpeakersData";
 function App() {
   return (
     <div className="App">
-      <Overview icon={faUserGroup} title="650 Participants">
-        Quisque ut libero sapien. Integer tellus nisl, efficitur sed dolor at,
-        vehicula finibus massa. Sed tincidunt metus sed eleifend suscipit.
-      </Overview>
+      <div style={{ display: "flex" }}>
+        {speakerData.map(item => {
+          return (
+            <CreativeSpeakers name={item.name} img={item.img} alt={item.alt} Job={item.job} key={item.id} />
+          )
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/design-system/components/CreativeSpeakers/SpeakersData.jsx
@@ -0,0 +1,45 @@
+import img1 from "../../../shared/assets/speakers/speakers-img1.jpg";
+import img2 from "../../../shared/assets/speakers/speakers-img2.jpg";
+import img3 from "../../../shared/assets/speakers/speakers-img3.jpg";
+import img4 from "../../../shared/assets/speakers/speakers-img4.jpg";
+import img5 from "../../../shared/assets/speakers/speakers-img5.jpg";
+
+const speakerData = [
+  {
+    id: 1,
+    img: img1,
+    alt: "imag1",
+    name: "Ahmed",
+    job: "Quisque1",
+  },
+  {
+    id: 2,
+    img: img2,
+    alt: "imag2",
+    name: "Mohamed",
+    job: "Quisque2",
+  },
+  {
+    id: 3,
+    img: img3,
+    alt: "imag3",
+    name: "Hassan",
+    job: "Quisque3",
+  },
+  {
+    id: 4,
+    img: img4,
+    alt: "imag4",
+    name: "Ibrahim",
+    job: "Quisque4",
+  },
+  {
+    id: 5,
+    img: img5,
+    alt: "imag5",
+    name: "Ali",
+    job: "Quisque5",
+  },
+];
+
+export default speakerData;
